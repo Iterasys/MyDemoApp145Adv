e1b0574d333f0c29977e038690d95896463259e1
Iterasys <contato@iterasys.com>
2025-11-26 22:17:25 -0300

    first commit

---
+++ features/step-definitions/comprar_produto.steps.js
@@ -0,0 +1,30 @@
+import { Given, When, Then } from "@wdio/cucumber-framework"
+
+
+Given("que estou na tela de Produtos", async() => {
+
+})
+
+When("na {string} seleciono o produto na posicao {string}", async(rolagem, index) => {
+
+})
+
+Then("verifico o {string} e o {string}", async() => {
+
+})
+
+When("adiciono o produto no carrinho", async() => {
+
+})
+
+Then("exibe o numero 1 no icone do carrinho", async() => {
+
+})
+
+When("clico no carrinho", async() => {
+
+})
+
+Then("verifico o {string} e o {string} no carrinho", async() => {
+    
+})
